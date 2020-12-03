a07addf6ee593c588750b2f461a283efd2d2692a
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-12-03 13:29:03 -0500

    Update web.js

--- web.js
+++ web.js
@@ -89,6 +89,7 @@ function generateTextArea(banListCards) {
     var categoryCards = banListCards.filter(a => a.category == category);
     for(var i=0; i< categoryCards.length; i++) {
       var card = categoryCards[i];
+      var color = cards.colors.length == 1 ? card.colors.join("") : "gold";
       textFill += `<div class="textItem ${card.colors.join(" ")}">${card.name}</div>`;
     }
     textFill += "</div></div>"
