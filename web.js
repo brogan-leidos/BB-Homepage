1fde6e6a2c3b741ea7400b2ad44a81a55144cd94
brogan-leidos <67585353+brogan-leidos@users.noreply.github.com>
2020-12-03 09:33:08 -0500

    Update web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ export default () => {
 }
 
 function fetchBanlist() {
-  fetch(banListUrl)
+  fetch(banListUrl, {mode: 'cors'})
     .then(response => response.json())
     .then(fetchData => {
       console.log(fetchData);
